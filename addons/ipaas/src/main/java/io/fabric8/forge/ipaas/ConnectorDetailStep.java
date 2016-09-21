1cac834ca3dc5317902e579a171f8e6fdc77f413
Claus Ibsen <claus.ibsen@gmail.com>
2016-09-21 17:15:00 +0200

    Maven connector plugin

--- addons/ipaas/src/main/java/io/fabric8/forge/ipaas/ConnectorDetailStep.java
+++ addons/ipaas/src/main/java/io/fabric8/forge/ipaas/ConnectorDetailStep.java
@@ -1,28 +1,32 @@
 /**
- *  Copyright 2005-2015 Red Hat, Inc.
- *
- *  Red Hat licenses this file to you under the Apache License, version
- *  2.0 (the "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- *  implied.  See the License for the specific language governing
- *  permissions and limitations under the License.
+ * Copyright 2005-2015 Red Hat, Inc.
+ * <p>
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 package io.fabric8.forge.ipaas;
 
 import javax.inject.Inject;
 
+import io.fabric8.forge.addon.utils.MavenHelpers;
 import io.fabric8.forge.ipaas.dto.ComponentDto;
 import io.fabric8.forge.ipaas.dto.ConnectionCatalogDto;
 import org.apache.camel.catalog.CamelCatalog;
 import org.jboss.forge.addon.dependencies.Dependency;
 import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
+import org.jboss.forge.addon.maven.plugins.ExecutionBuilder;
+import org.jboss.forge.addon.maven.plugins.MavenPluginBuilder;
+import org.jboss.forge.addon.maven.projects.MavenPluginFacet;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.dependencies.DependencyInstaller;
 import org.jboss.forge.addon.projects.facets.ResourcesFacet;
@@ -104,6 +108,35 @@ public class ConnectorDetailStep extends AbstractIPaaSProjectCommand {
         if (labels.getValue() != null) {
             catalog.setLabels(labels.getValue().split(","));
         }
+
+        // add maven plugin stuff
+        /*
+          <build>
+    <plugins>
+      <plugin>
+        <groupId>io.fabric8.django</groupId>
+        <artifactId>connector-maven-plugin</artifactId>
+        <version>2.3-SNAPSHOT</version>
+        <executions>
+          <execution>
+            <goals>
+              <goal>jar</goal>
+            </goals>
+          </execution>
+        </executions>
+      </plugin>
+    </plugins>
+  </build>
+         */
+
+
+        // add connector-maven-plugin
+        String version = MavenHelpers.getVersion("io.fabric8.django", "ipaas");
+        MavenPluginFacet pluginFacet = project.getFacet(MavenPluginFacet.class);
+        MavenPluginBuilder plugin = MavenPluginBuilder.create()
+                .setCoordinate(createCoordinate("io.fabric8.django", "connector-maven-plugin", version));
+        plugin.addExecution(ExecutionBuilder.create().setId("connector").addGoal("jar"));
+        pluginFacet.addPlugin(plugin);
 
         // marshal DTO
         String json = toJson(catalog);
