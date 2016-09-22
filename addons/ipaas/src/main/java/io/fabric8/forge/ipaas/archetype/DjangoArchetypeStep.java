465e360ee1805f158bd79767fbb3e767b98bbd17
Claus Ibsen <claus.ibsen@gmail.com>
2016-09-22 09:56:40 +0200

    Add connector as project-new type

---
+++ addons/ipaas/src/main/java/io/fabric8/forge/ipaas/archetype/DjangoArchetypeStep.java
@@ -0,0 +1,65 @@
+/**
+ *  Copyright 2005-2015 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.fabric8.forge.ipaas.archetype;
+
+import java.util.List;
+import javax.inject.Inject;
+
+import com.google.common.base.Objects;
+import io.fabric8.forge.addon.utils.VersionHelper;
+import io.fabric8.utils.Strings;
+import org.apache.maven.archetype.catalog.Archetype;
+import org.jboss.forge.addon.maven.projects.archetype.ui.ConstantArchetypeSelectionWizardStep;
+
+public abstract class DjangoArchetypeStep extends ConstantArchetypeSelectionWizardStep {
+
+    @Inject
+    private DjangoArchetypeCatalogFactory catalogFactory;
+
+    public DjangoArchetypeStep() {
+        setArchetypeGroupId("io.fabric8.archetypes");
+    }
+
+    @Override
+    protected void setArchetypeArtifactId(String archetypeArtifactId) {
+        super.setArchetypeArtifactId(archetypeArtifactId);
+        if (Strings.isNotBlank(archetypeArtifactId)) {
+            if (Strings.isNullOrBlank(getArchetypeVersion())) {
+                String version = null;
+                if (catalogFactory != null) {
+                    List<Archetype> archetypes = catalogFactory.getArchetypeCatalog().getArchetypes();
+                    for (Archetype archetype : archetypes) {
+                        if (Objects.equal(archetype.getArtifactId(), archetypeArtifactId)) {
+                            version = archetype.getVersion();
+                        }
+                    }
+                }
+                if (Strings.isNullOrBlank(version)) {
+                    version = VersionHelper.fabric8ArchetypesVersion();
+                }
+                if (version != null) {
+                    setArchetypeVersion(version);
+                } else {
+                    throw new IllegalArgumentException("Could not find an archetype for id " + archetypeArtifactId
+                            + " in the archetype catalog " + catalogFactory + " or find version for environment variable " + VersionHelper.ENV_FABRIC8_ARCHETYPES_VERSION);
+                }
+            }
+        }
+    }
+
+
+
+}
