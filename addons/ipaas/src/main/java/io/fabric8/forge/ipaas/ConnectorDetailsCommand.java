6d08b530f8bedbd5318a51dbb880d415cb4f3c5e
Claus Ibsen <claus.ibsen@gmail.com>
2016-10-12 16:24:58 +0200

    Fixes #56 to have connector-details command to output json schema

--- addons/ipaas/src/main/java/io/fabric8/forge/ipaas/ConnectorDetailsCommand.java
+++ addons/ipaas/src/main/java/io/fabric8/forge/ipaas/ConnectorDetailsCommand.java
@@ -15,11 +15,16 @@
  */
 package io.fabric8.forge.ipaas;
 
+import java.util.List;
+import java.util.Properties;
 import javax.inject.Inject;
 
 import org.apache.camel.catalog.CamelCatalog;
+import org.jboss.forge.addon.dependencies.Dependency;
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
-import org.jboss.forge.addon.projects.facets.ResourcesFacet;
+import org.jboss.forge.addon.maven.projects.facets.MavenDependencyFacet;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.input.UIInput;
@@ -30,7 +35,10 @@ import org.jboss.forge.addon.ui.result.Results;
 import org.jboss.forge.addon.ui.util.Categories;
 import org.jboss.forge.addon.ui.util.Metadata;
 
-@FacetConstraint({ResourcesFacet.class})
+import static io.fabric8.forge.ipaas.helper.CamelComponentDependencyHelper.extractComponentJavaType;
+import static io.fabric8.forge.ipaas.helper.CamelComponentDependencyHelper.loadComponentProperties;
+
+@FacetConstraint({MavenDependencyFacet.class})
 public class ConnectorDetailsCommand extends AbstractIPaaSProjectCommand {
 
     @Inject
@@ -48,6 +56,11 @@ public class ConnectorDetailsCommand extends AbstractIPaaSProjectCommand {
     }
 
     @Override
+    public void initializeUI(UIBuilder builder) throws Exception {
+        builder.add(name);
+    }
+
+    @Override
     public boolean isEnabled(UIContext context) {
         boolean answer = super.isEnabled(context);
         if (answer) {
@@ -61,12 +74,48 @@ public class ConnectorDetailsCommand extends AbstractIPaaSProjectCommand {
     @Override
     public Result execute(UIExecutionContext context) throws Exception {
         String scheme = name.getValue();
+
+        // maybe the connector has already been added to the catalog
         String json = camelCatalog.componentJSonSchema(scheme);
+        if (json == null) {
+            // discover classpath and find all the connectors and add them to the catalog
+            Project project = getSelectedProjectOrNull(context.getUIContext());
+            if (project != null) {
+                discoverCamelConnectorOnClasspathAndAddToCatalog(camelCatalog, project);
+            }
+            // load schema again
+            json = camelCatalog.componentJSonSchema(scheme);
+        }
 
         if (json != null) {
             return Results.success(json);
         } else {
             return Results.fail("Connector " + scheme + " not found");
+        }
+    }
+
+    private void discoverCamelConnectorOnClasspathAndAddToCatalog(CamelCatalog camelCatalog, Project project) {
+        // find the dependency again because forge don't associate artifact on the returned dependency when installed
+        MavenDependencyFacet facet = project.getFacet(MavenDependencyFacet.class);
+        List<Dependency> list = facet.getEffectiveDependencies();
+
+        for (Dependency dep : list) {
+            Properties properties = loadComponentProperties(dep);
+            if (properties != null) {
+                String components = (String) properties.get("components");
+                if (components != null) {
+                    String[] part = components.split("\\s");
+                    for (String scheme : part) {
+                        if (!camelCatalog.findComponentNames().contains(scheme)) {
+                            // find the class name
+                            String javaType = extractComponentJavaType(dep, scheme);
+                            if (javaType != null) {
+                                camelCatalog.addComponent(scheme, javaType);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
